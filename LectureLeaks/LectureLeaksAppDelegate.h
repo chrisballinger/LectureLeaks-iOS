e4c88aae5c29a05e9f32638a294f026744db881c
Chris Ballinger <chrisballinger@gmail.com>
2011-06-06 21:31:24 -0400

    Initial Commit

---
+++ LectureLeaks/LectureLeaksAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  LectureLeaksAppDelegate.h
+//  LectureLeaks
+//
+//  Created by Christopher Ballinger on 6/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class LectureLeaksViewController;
+
+@interface LectureLeaksAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet LectureLeaksViewController *viewController;
+
+@end
---
+++ LectureLeaks/LectureLeaksViewController.h
@@ -0,0 +1,15 @@
+//
+//  LectureLeaksViewController.h
+//  LectureLeaks
+//
+//  Created by Christopher Ballinger on 6/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LectureLeaksViewController : UIViewController {
+    
+}
+
+@end
