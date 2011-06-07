924415837688f9ec409c0ab126b68f17ac5061aa
Chris Ballinger <chrisballinger@gmail.com>
2011-06-06 22:56:32 -0400

    Added some basic UI

---
+++ LectureLeaks/AboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  AboutViewController.h
+//  LectureLeaks
+//
+//  Created by Christopher Ballinger on 6/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AboutViewController : UIViewController {
+    
+}
+
+@end
