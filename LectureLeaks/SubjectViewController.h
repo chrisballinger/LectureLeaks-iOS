5d2ee5c3888f26fd353cdc8ef4137db902d2b99b
Chris Ballinger <chrisballinger@gmail.com>
2011-06-14 01:00:21 -0400

    Subject view (list courses)

---
+++ LectureLeaks/SubjectViewController.h
@@ -0,0 +1,16 @@
+//
+//  SubjectViewController.h
+//  LectureLeaks
+//
+//  Created by Christopher Ballinger on 6/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SubjectViewController : UITableViewController {
+    
+}
+
+@end
